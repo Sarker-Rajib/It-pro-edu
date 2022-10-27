a17bc93a983d2e11f3cef4ce56c90b89620f4687
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-27 12:36:03 +0600

    faq Q A added

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -6,8 +6,40 @@ const Faq = () => {
       <div className="pt-3">
          <Container>
             <div className="my-2 bg-nav p-3 rounded border border-info">
-               <h3 className='fs-4'>Q. What is `Cor's` ?</h3>
-               <p><span className='fs-5'># Answer : </span> Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.</p>
+               <h3 className='fs-5'>Q. Can I learn at my own pace?</h3>
+               <p>This is another common question that people have about online courses. There are usually self-paced programs and traditional classroom or meeting-style programs available. Today’s world is full of busy people that want to better themselves, so the self-paced programs are popular. Whether or not you provide this option, be sure to answer this question for people and explain the pros and cons of each type of learning. As a bonus, you can even invite them to contact you for more information or to discuss what their options are.</p>
+            </div>
+
+            <div className="my-2 bg-nav p-3 rounded border border-info">
+               <h3 className='fs-5'>Q. Are you accredited or licensed by educational organizations?</h3>
+               <p>There isn’t always a need for licensing or accreditation for online courses. However, some programs do require it. Either way, people will usually ask what type of training or background you have, as well as any licensing that’s available if that’s relevant. Make sure that you answer this question by telling them if you do hold any licenses or accreditation, as well as by explaining whether or not those are needed, and why. It gives them the confidence that you have the right experience and boosts your credibility because you’re offering additional information for the sake of education alone. </p>
+            </div>
+
+            <div className="my-2 bg-nav p-3 rounded border border-info">
+               <h3 className='fs-5'>Q. Does online learning really work?</h3>
+               <p>There’s always a skeptic, and when it comes to online learning, there are often several of them. Everyone wants to know if online courses “really work”—and that’s a hard question to answer because there are several factors to consider. On a general basis, yes, online learning can be effective. However, that depends on whether the courses are set up properly, the provider is educated and experienced, and if the person taking the courses does well in a virtual learning environment. You should be able to explain these things to people so that they know what they’re getting into. Then, you can invite them to contact you to discuss whether it would be right for them. </p>
+            </div>
+
+            <div className="my-2 bg-nav p-3 rounded border border-info">
+               <h3 className='fs-5'>Q. What types of assignments do you give?</h3>
+               <p>Like knowing the format of online learning, people often want to know the type of work they’ll be doing when they take online courses. Be sure to provide a glimpse of what you offer here and link to previous projects or assignments if you can. You can also link to course or program pages. As long as you explain to people how the courses are taught and what they can expect in terms of coursework, you’ll be helping them make a better decision about their educational needs. </p>
+            </div>
+
+            <div className="my-2 bg-nav p-3 rounded border border-info">
+               <h3 className='fs-5'>Q. What are the requirements to take the courses or sign up with your website?</h3>
+               <p>People will want to know how to get started. You can link them to the pages on your website that help them through that process, but you’ll also want to take the time to explain it to them here. If you have certain enrollment windows, for example, list those. If payment or deposits are required, explain that, as well. Make sure that people know exactly what they need to do to take online courses from you, no matter what those courses might be. </p>
+            </div>
+
+            <div className="my-2 bg-nav p-3 rounded border border-info">
+               <h3 className='fs-5'>Q. What are the technology requirements?</h3>
+               <p>In the same vein, people need to know whether their computers are up to snuff. You’ll want to explain all the software that you use and what kind of technology people need to take your online courses. If there are certain programs or applications used, put those here. If you have the option to provide technology or direct people to the appropriate purchase, you should do that here, as well. Reassure people that you can help them with the requirements and that they can contact you to discuss whether their system meets the demands of your online courses. </p>
+            </div>
+
+            <div className="my-2 bg-nav p-3 rounded border border-info">
+               <h3 className='fs-5'>Q. How long does each course take?</h3>
+               <p>Some online courses are just a few hours long, while others last a few months. Depending on the type of online learning that you offer, your courses may have their own variable lengths. Explain to people the types of courses that you have available and what the time frame is for each one. If you offer self-paced learning, this is another place to reiterate that and reassure people that you can help them learn on their own time.
+
+                  It could also be helpful to link to your course catalog or course pages here so that people can see the exact specifications of the course(s) they are considering taking from your website. </p>
             </div>
          </Container>
       </div>
