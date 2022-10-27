21257068cedc69390230ec782266c790311e4703
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-27 19:22:13 +0600

    'spinned'

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,18 +1,29 @@
 import React, { useContext } from 'react';
+import { Container } from 'react-bootstrap';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/Authentication/AuthProvider';
+import { Dna } from 'react-loader-spinner';
 
 const PrivateRoute = ({ children }) => {
-   const {user, loading} = useContext(AuthContext);
+   const { user, loading } = useContext(AuthContext);
 
    const location = useLocation();
 
    if (loading) {
-      return <p>Loading</p>     
+      const spinner = <Container className="text-center"><Dna
+         visible={true}
+         height="150"
+         width="150"
+         ariaLabel="dna-loading"
+         wrapperStyle={{}}
+         wrapperClass="dna-wrapper"
+      />
+      </Container>
+      return spinner;
    }
 
    if (!user) {
-      return <Navigate to='/login' state={{from: location}} replace></Navigate>
+      return <Navigate to='/login' state={{ from: location }} replace></Navigate>
    }
 
    return children;
